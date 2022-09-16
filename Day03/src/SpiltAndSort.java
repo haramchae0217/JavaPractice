3c3b349ef28cd1858aad51e8ed2a572c97938e1a
Chae Haram <djvemfla2@gmail.com>
2022-09-16 20:33:58 +0900

    자바 3주차 수업 예제 및 과제

--- Day03/src/SpiltAndSort.java
+++ Day03/src/SpiltAndSort.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class SpiltAndSort {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("문장을 입력하세요>> ");
+			String a = sc.nextLine();
+			
+			if (a.equals("exit")) {
+				break;
+			}
+			
+			String s[] = a.split(" ");
+			System.out.println("어절의 개수는 " + s.length);
+			Arrays.sort(s);
+			for (int i=0; i<s.length; i++) {
+				System.out.println(s[i]);	
+			}
+		}
+	}
 }
--- Day03/src/StringRotation.java
+++ Day03/src/StringRotation.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class StringRotation {
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] arr;
+		
+		System.out.println("문장을 입력하세요.");
+		String a = sc.nextLine();
+		arr = a.split(" ");
+		
+		for(int i=0; i<a.length(); i++) {
+			String f = a.substring(0,1);
+			String res = a.substring(1);
+			a = res+f;
+			System.out.println(a);
+		}
+	}
 }
