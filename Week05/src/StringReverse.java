428a50f3e2520a6248a76b3367aceb77ed561886
Chae Haram <djvemfla2@gmail.com>
2022-09-29 15:28:36 +0900

    5주차 실습 4

---
+++ Week05/src/StringReverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class StringReverse {
+	public static void main(String[] args) {
+		String s = "abcdef";
+		
+		LinkedList<Character> stack = new LinkedList<Character>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			stack.push(s.charAt(i) );
+		}
+		
+		while( stack.isEmpty() == false ) {
+			System.out.print( stack.pop() );
+		}
+		System.out.println();
+	}
+}
