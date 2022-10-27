766aa318bae387ff0ea4256e55c7c6c210c3af7e
Chae Haram <djvemfla2@gmail.com>
2022-10-27 14:56:31 +0900

    예제 -1

---
+++ Week09/src/PrintException1.java
@@ -0,0 +1,12 @@
+public class PrintException1 {
+	public static void main(String[] args) {
+		try {
+			int a = 100 / 0;
+		} catch (ArithmeticException ex) {
+			System.out.println(ex);
+		} finally {
+			System.out.println("Finally block");
+		}
+		System.out.println("End of main");
+	}
+}
