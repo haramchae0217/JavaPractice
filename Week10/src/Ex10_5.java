5cab63c71c1406b58262db2548fedc86dd015730
Chae Haram <djvemfla2@gmail.com>
2022-11-03 22:54:43 +0900

    실습 -8

---
+++ Week10/src/Ex10_5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Ex10_5 {
+	public static void main(String[] args) {
+		FileReader fin = null;
+		try {
+			fin = new FileReader("konkuk.txt");
+			int c;
+			while((c = fin.read()) != -1) {
+				c += 3;
+				System.out.print((char) c);
+			}
+			fin.close();
+		} catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+	}
+}
