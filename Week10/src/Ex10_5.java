bc8ed70ccf54ed470f8e538a22a765f7dc17e34a
Chae Haram <djvemfla2@gmail.com>
2022-11-10 14:52:09 +0900

    11주차 수업

--- Week10/src/Ex10_5.java
+++ Week10/src/Ex10_5.java
@@ -8,8 +8,20 @@ public class Ex10_5 {
 			fin = new FileReader("konkuk.txt");
 			int c;
 			while((c = fin.read()) != -1) {
-				if( c != 32) {
+//				if(c != 32) {
+//					c += 3;
+//				}
+				if(Character.isLowerCase(c)) {
 					c += 3;
+					if(!Character.isLowerCase(c)) {
+						c -= 26;
+					}
+				}
+				if(Character.isUpperCase(c)) {
+					c += 3;
+					if(!Character.isUpperCase(c)) {
+						c -= 26;
+					}
 				}
 				System.out.print((char) c);
 			}
