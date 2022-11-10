9c511193bec39c310e5c665076cb9da18fcc6a2b
Chae Haram <djvemfla2@gmail.com>
2022-11-10 14:54:01 +0900

    실습 5

---
+++ Week11/src/OutputStreamWriterEx.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class OutputStreamWriterEx {
+	public static void main(String[] args) {
+		OutputStreamWriter out = null;
+		FileOutputStream fout = null;
+		try {
+			fout = new FileOutputStream("/Users/Haram/Project/JavaPractice/Week11/hangul.txt");
+			out = new OutputStreamWriter(fout,"MS949");
+			out.write("가나다라마바사아자차카타파하");
+			out.close();
+			fout.close();
+		} catch(IOException e) {
+			System.out.println("입출력 오류.");
+		}
+	}
+}
